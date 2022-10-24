d383848668dc2585754a3e2de72ef886f74ce084
Arko Elsenaar <a.elsenaar@situatiewin.nl>
2022-10-24 20:07:41 +0200

    Adds version support

--- src/MessageParser.php
+++ src/MessageParser.php
@@ -95,16 +95,26 @@ class MessageParser {
     public function __invoke($message)
     {
         $content = strtolower($message->content);
+
         // Grab the first 4 chars for our trigger
         $trigger = substr($content, 0, 5);
+
         // Grab everything after the first 5 chars
         $query = substr($content, 5);
+
+        // Check if version is available in command
+        $pattern = '/\b([6-9]\.x)\b/'; // only match 6.x to 9.x
+        preg_match($pattern, $query, $matches);
+        if ($version = $matches[0] ?? "9.x") // default to 9.x if no version is given
+            $query = preg_replace($pattern, '', $query);
+
+        $query = trim($query);
 
         if($trigger === "docs ")
         {
             if(in_array($query, $this->docs)){
 
-                return "<https://laravel.com/docs/$query>";
+                return "<https://laravel.com/docs/$version/$query>";
             }
         }
 
